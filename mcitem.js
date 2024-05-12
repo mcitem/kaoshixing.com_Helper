54b81ae2ab703c024e85597ab17e1b5c6845ac8e
MCITEM <124600270+mcitem@users.noreply.github.com>
2024-05-12 16:55:47 +0800

    Update license

--- mcitem.js
+++ mcitem.js
@@ -7,6 +7,7 @@
 // @match        https://v.kaoshixing.com/exam/pc/course/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kaoshixing.com
 // @grant        none
+// @license      AGPLv3
 // ==/UserScript==
 
 (function() {
